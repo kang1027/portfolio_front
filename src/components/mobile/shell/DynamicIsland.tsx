cff2babb131bd55a4c18017726390fb53c3dc49a
kang1027 <kang3171611@naver.com>
2026-05-02 14:39:11 +0900

    polish(mobile): 100dvh, locale-unified date, drop unused css/layout prop

--- src/components/mobile/shell/DynamicIsland.tsx
+++ src/components/mobile/shell/DynamicIsland.tsx
@@ -3,7 +3,6 @@ import { motion } from "framer-motion";
 export default function DynamicIsland() {
   return (
     <motion.div
-      layout
       className="absolute top-2.5 left-1/2 -translate-x-1/2 h-8 w-32 bg-black rounded-full z-30"
       transition={{ type: "spring", stiffness: 350, damping: 30 }}
     />
