427f1cb8b2509dd46773ad295dc7f5c95cc088ec
kang1027 <kang3171611@naver.com>
2026-05-02 15:27:30 +0900

    polish(mobile widgets): album art fallback + skeleton, photo lazy, trim WidgetPage padding

--- src/components/mobile/home/WidgetPage.tsx
+++ src/components/mobile/home/WidgetPage.tsx
@@ -6,7 +6,7 @@ import WeatherSmall from "../widgets/WeatherSmall";
 
 export default function WidgetPage() {
   return (
-    <div className="px-4 pt-16 grid grid-cols-4 gap-3 pb-44">
+    <div className="px-4 pt-16 grid grid-cols-4 gap-3 pb-8">
       <PhotoLarge />
       <WeatherSmall />
       <CalendarSmall />
